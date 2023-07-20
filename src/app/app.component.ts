6b6e899e7184156a1987c0b7b31660a482cd5e56
Adnan Abbasi <adnan_abbasi@yahoo.com>
2023-07-20 15:24:59 -0400

    Frontend patientapp & backend nodejsapits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PatientService } from './service/patient.service';
+import { CustomHttpResponse } from './interface/httpResponse';
+import { Patient } from './interface/patient';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'patientapp';
+export class AppComponent implements OnInit{
+  httpResponse: CustomHttpResponse | undefined;
+  patients: Patient[] | undefined;
+
+  constructor(private patientService: PatientService) {}
+
+
+  ngOnInit(): void {
+    this.patientService.getPatients().subscribe(
+      response => {
+        this.httpResponse = response;
+        this.patients = response.data;
+        console.log(response)
+      },
+      error => console.log(error)
+    )
+  }
 }
